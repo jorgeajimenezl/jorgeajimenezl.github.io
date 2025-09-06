af1e4971d93bf756b69216474b446e7cc6996f13
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-09-06 13:34:02 +0200

    feat: add migrations directory to database configuration and update migration number comment

--- worker/migrations/0001_init.sql
+++ worker/migrations/0001_init.sql
@@ -1,3 +1,5 @@
+-- Migration number: 0001 	 2025-09-06T11:32:19.972Z
+
 CREATE TABLE IF NOT EXISTS comments (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   slug TEXT NOT NULL,
@@ -6,8 +8,7 @@ CREATE TABLE IF NOT EXISTS comments (
   body_html TEXT NOT NULL DEFAULT '',
   created_at INTEGER NOT NULL,
   ip_hash TEXT NOT NULL,
-  ua TEXT,
+  ua TEXT
 );
 
-CREATE INDEX IF NOT EXISTS idx_comments_slug_created
-ON comments (slug, created_at DESC);
+CREATE INDEX IF NOT EXISTS idx_comments_slug_created ON comments (slug, created_at DESC);
